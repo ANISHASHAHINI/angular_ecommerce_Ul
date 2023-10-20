f5011498a2b17790c078b9dc27259505f089fbe9
ANISHASHAHINI <anishashahini2002@gmail.com>
2023-10-20 19:14:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBasics';
+  title = 'Angular First App';
+  messge = 'This is my starting angular app'
 }
+ 
